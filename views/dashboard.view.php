418326395c44daa5f11ca40cd00629411cfd0a7a
Antoine Sueur <antoine.sueur17@gmail.com>
2022-03-11 11:40:29 +0100

    Responsive du dashboard

--- views/dashboard.view.php
+++ views/dashboard.view.php
@@ -3,26 +3,26 @@
 ob_start();
 
 ?>
-    <section class="round shadow-lg p-5 container-mobile dashboard d-flex">
+    <section class="round shadow-lg dashboard-container container-mobile flex-column flex-lg-row dashboard d-flex">
         <?php if(isset($_GET['onglet'])) { ?>
             <input id="onglet" type="hidden" value="<?= $_GET['onglet'] ?>">
         <?php } ?>
-        <div id="menuDashboard" class="myBackground largeur15 text-white fw-bold border p-3 rounded">
-            <h1 class="mb-5 fs-3 text-center pb-2 border-bottom">Dashboard</h1>
-            <div class="d-flex flex-column justify-content-center">
-                <p id="vueEnsemble"><i class="far fa-chart-bar fs-3 mb-3 me-2"></i> Vue d'ensemble</p>
-                <p id="vueArticles"><i class="far fa-newspaper fs-3 mb-3 me-2"></i> Articles</p>
-                <p id="vueCategories"><i class="far fa-bookmark fs-3 mb-3 me-2"></i> Categories</p>
-                <p id="vueMembres"><i class="far fa-user fs-3 mb-3 me-2"></i> Membres</p>
+        <div id="menuDashboard" class="myBackground text-white fw-bold border p-lg-3 rounded">
+            <h1 class="mb-5 fs-3 text-center pb-2 border-bottom d-none d-lg-block">Dashboard</h1>
+            <div class="d-flex flex-lg-column pt-3 pt-lg-0 align-items-center align-items-lg-start justify-content-around">
+                <p id="vueEnsemble" class="d-lg-flex"><i class="far fa-chart-bar fs-3 mb-lg-3 me-lg-2 text-center"></i> <span class="d-none d-lg-block">Vue d'ensemble</span></p>
+                <p id="vueArticles" class="d-lg-flex"><i class="far fa-newspaper fs-3 mb-lg-3 me-lg-2"></i> <span class="d-none d-lg-block">Articles</span></p>
+                <p id="vueCategories" class="d-lg-flex"><i class="far fa-bookmark fs-3 mb-lg-3 me-lg-2"></i> <span class="d-none d-lg-block">Categories</span></p>
+                <p id="vueMembres" class="d-lg-flex"><i class="far fa-user fs-3 mb-lg-3 me-lg-2"></i> <span class="d-none d-lg-block">Membres</span></p>
             </div>
         </div>
-        <div id="contenuDashboard" class="largeur80 ms-5">
+        <div id="contenuDashboard" class="largeur80 m-auto m-lg-0 ms-lg-5">
             <div id="overview">
-                <h2 class="mb-3"><i class="far fa-chart-bar fs-2 mb-3 me-2"></i>Vue d'ensemble</h2>
-                <div class="d-flex justify-content-between flex-wrap">
+                <h2 class="mb-3 text-center"><i class="far fa-chart-bar fs-2 mb-5 me-2 mt-4"></i>Vue d'ensemble</h2>
+                <div class="d-flex justify-content-between flex-column flex-lg-row flex-wrap">
 
                     <div id="graphViews" class="largeur45 border p-2"></div>
-                    <div id="graphMembers" class="largeur45 border p-2"></div>
+                    <div id="graphMembers" class="largeur45 border p-2 mt-5 mt-lg-0"></div>
                     <div id="graphContainer" class="largeur45 border p-2 mt-5"></div>
                     <div id="" class="largeur45 p-2 mt-5">
                         <h2 class="mb-4">Derniers commentaires :</h2>
@@ -38,7 +38,7 @@ ob_start();
                                     <p class="fw-bold text-center"><?= $commentaire['pseudo'] ?></p>
                                 </div>
 
-                                <div class="d-flex flex-column ms-3">
+                                <div class="d-flex flex-column ms-2 ms-lg-3">
                                     <div class="d-flex">
                                         <p class="ms-3"><a class="text-danger"
                                                            href="index.php?route=article&action=deleteComment&commentId=<?= $commentaire['id_commentaire'] ?>"><i
@@ -55,8 +55,8 @@ ob_start();
                     </div>
                 </div>
             </div>
-            <div id="sectionArticles" class="d-none ms-5">
-                <h2 class="mb-3"><i class="far fa-newspaper fs-2 mb-3 me-2"></i>Articles</h2>
+            <div id="sectionArticles" class="d-none ms-lg-5">
+                <h2 class="mb-3 text-center"><i class="far fa-newspaper fs-2 mb-3 me-2 mt-4"></i>Articles</h2>
                 <?= isset($_SESSION['info']['article']) ? '<p class="text-danger">' . $_SESSION['info']['article'] . '</p>' : '' ?>
                 <?= isset($_SESSION['info']['articlemodif']) ? '<p class="text-success">' . $_SESSION['info']['articlemodif'] . '</p>' : '' ?>
                 <?= isset($_SESSION['info']['articleadd']) ? '<p class="text-success">' . $_SESSION['info']['articleadd'] . '</p>' : '' ?>
@@ -96,8 +96,8 @@ ob_start();
                     } ?>
                 </table>
             </div>
-            <div id="sectionCategories" class="d-none ms-5">
-                <h2 class=" mb-4" id="categorie"><i class="far fa-bookmark fs-2 mb-3 me-2"></i>Categories</h2>
+            <div id="sectionCategories" class="d-none ms-lg-5">
+                <h2 class=" mb-4 text-center" id="categorie"><i class="far fa-bookmark fs-2 mb-3 me-2 mt-4"></i>Categories</h2>
                 <?= isset($_SESSION['info']['categorie']) ? '<p class="text-danger">' . $_SESSION['info']['categorie'] . '</p>' : '' ?>
                 <?= isset($_SESSION['success']) ? '<p class="text-success">' . $_SESSION['success'] . '</p>' : '' ?>
                 <div class="toble">
@@ -130,10 +130,10 @@ ob_start();
                         } ?>
                     </table>
                 </div>
-                <div id="graphCategories" class="border rounded p-2 mt-5"></div>
+                <div id="graphCategories" class="border rounded p-2 mt-5 mb-5"></div>
             </div>
-            <div id="sectionMembres" class="d-none ms-5">
-                <h2 class="mb-5" id="membre"><i class="far fa-user fs-2 mb-3 me-2"></i>Membres</h2>
+            <div id="sectionMembres" class="d-none ms-lg-5">
+                <h2 class="mb-5 text-center" id="membre"><i class="far fa-user fs-2 mb-3 me-2 mt-4"></i>Membres</h2>
                 <?= isset($_SESSION['info']['membre']) ? '<p class="text-danger">' . $_SESSION['info']['membre'] . '</p>' : '' ?>
                 <table class="table">
                     <thead>
